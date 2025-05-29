caad84eaf3dd2867f7d809c0db0b8d87dac1889a
Scartissue <68987475+imscartissue@users.noreply.github.com>
2025-05-29 23:17:10 +0545

    Update CreatePlaylistBtn.tsx

--- src/components/CreatePlaylistBtn.tsx
+++ src/components/CreatePlaylistBtn.tsx
@@ -17,7 +17,7 @@ const CreatePlaylistBtn = () => {
     }
 
     return (
-        <button onClick={handleClick} className="bg-red-700 rounded-2xl p-1 sm:p-2 hover:bg-yellow-800 hover:cursor-pointer transition-colors duration-50 ease-in-out text-center disabled:cursor-not-allowed" disabled={isDisabled}>Create playlist</button>
+        <button onClick={handleClick} className="bg-yellow-700 rounded-2xl p-1 sm:p-2 hover:bg-yellow-800 hover:cursor-pointer transition-colors duration-50 ease-in-out text-center disabled:cursor-not-allowed" disabled={isDisabled}>Create playlist</button>
     );
 };
 
